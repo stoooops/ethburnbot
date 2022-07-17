039d1fc92d3084864b73a346a8580849c579c480
Cory Gabrielsen <corygabrielsen@gmail.com>
2022-07-17 03:15:57 -0700

    Update log formatter

--- bin/run_puller.py
+++ bin/run_puller.py
@@ -44,7 +44,7 @@ def setup_logging() -> None:
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s %(levelname)7s (%(threadName)9s) [%(name)s:%(lineno)s] %(message)s")
+    formatter = logging.Formatter("%(asctime)s %(levelname)7s %(message)s [%(name)s:%(lineno)s]")
     handler.setFormatter(formatter)
     root.addHandler(handler)
 
