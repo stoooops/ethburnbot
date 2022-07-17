da40c74c7fcbd9f8e344d14dcf3f485241df8fee
Cory Gabrielsen <corygabrielsen@gmail.com>
2022-07-17 01:51:08 -0700

    Update logging

--- bin/run_puller.py
+++ bin/run_puller.py
@@ -10,6 +10,7 @@ from threading import Thread
 
 from eth.core.ethereum_client import EthereumClient, GethClient
 from eth.core.puller import BlockPuller
+from eth.types.block import DetailedBlock
 from potpourri.python.ethereum.constants import LONDON
 
 LOG = logging.getLogger(__name__)
@@ -48,6 +49,11 @@ def setup_logging() -> None:
     root.addHandler(handler)
 
 
+def log_progress(block: DetailedBlock, prefix: str = "") -> None:
+    prefix = f"{prefix}    " if prefix != "" else prefix
+    LOG.info(f"{prefix}block={block.number} time={block.timestamp_dt}")
+
+
 def run_puller(eth_addr: str, eth_port: int, use_cache: bool, block: int) -> None:
     eth_client: EthereumClient = GethClient(ip_addr=eth_addr, port=eth_port)
     block_puller: BlockPuller = BlockPuller(eth_client=eth_client)
@@ -58,7 +64,10 @@ def run_puller(eth_addr: str, eth_port: int, use_cache: bool, block: int) -> Non
     while _still_running():
         time.sleep(0)
         latest_block_number = block_puller.eth_blockNumber()
-        LOG.info(f"Latest block number: {latest_block_number}")
+        block: DetailedBlock = block_puller.eth_getBlockByNumber(
+            block_num, cached=use_cache, prev_sha3_uncles=prev_sha3_uncles
+        )
+        log_progress(block, "latest")
 
         if start_block != latest_block_number:
             for block_num in range(start_block, latest_block_number):
@@ -66,18 +75,16 @@ def run_puller(eth_addr: str, eth_port: int, use_cache: bool, block: int) -> Non
                     block_num, cached=use_cache, prev_sha3_uncles=prev_sha3_uncles
                 )
                 prev_sha3_uncles = block._sha3_uncles
-
-                if block_num % 100 == 0:
-                    LOG.info(f"Cached block {block_num} @ {block.timestamp_dt}")
+                log_progress(block, prefix="cache")
 
                 if not _still_running():
-                    LOG.info("Exit Block Cacher")
+                    log_progress(block, prefix="exit block cacher")
                     return
 
         sleep_sec = 1 if datetime.now().minute in [59, 0, 1] else 20
         for _ in range(sleep_sec):
             if not _still_running():
-                LOG.info("Exit Block Cacher")
+                log_progress(block, prefix="exit block cacher")
                 return
             time.sleep(1)
         start_block = latest_block_number
